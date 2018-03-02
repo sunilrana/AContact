556634b8e9c56eb833c431fe265bdd5451917936
Sunil Rana <sunilrana@Sunils-MacBook-Pro.local>
2018-03-02 10:04:07 -0500

    bug fix

--- app/src/main/java/com/sunilrana/acontact/utils/ContactHelper.java
+++ app/src/main/java/com/sunilrana/acontact/utils/ContactHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
-import android.text.TextUtils;
 
 import com.sunilrana.acontact.model.ContactData;
 
@@ -37,30 +36,26 @@ public class ContactHelper {
                 if (isCursorValid(emails) && isCursorValid(phones)) {
                     String name = contactsCursor.getString(contactsCursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
 
-                    String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    mobileList.add(phone);
 
-                    while (phones.moveToNext()) {
-                        String phoneN = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        mobileList.add(phoneN);
-
-                    }
-                    phones.close();
-
-
-
-                    while (emails.moveToNext()) {
-
-                        String emAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
-                        emailList.add(emAddress);
+                    if (phones.moveToFirst()) {
+                        do {
+                            String phoneN = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            mobileList.add(phoneN);
+                        } while (phones.moveToNext());
+                        phones.close();
                     }
 
-                    emails.close();
 
+                    if (emails.moveToFirst()) {
+                        do {
+                            String emAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                            emailList.add(emAddress);
+                        } while (emails.moveToNext());
+                        emails.close();
+                    }
 
-
-                    if (!TextUtils.isEmpty(phone)) {
-                        contactData = new ContactData(contactId, name, mobileList , emailList);
+                    if (mobileList.size() > 0) {
+                        contactData = new ContactData(contactId, name, mobileList, emailList);
                     }
                 }
                 closeCursor(emails);
@@ -69,16 +64,16 @@ public class ContactHelper {
         }
         closeCursor(contactsCursor);
 
-       return contactData;
+        return contactData;
     }
 
     private Cursor getContactsCursor(Uri contactUri) {
         String sortOrder = ContactsContract.Contacts.SORT_KEY_PRIMARY;
-        String[] projection = { ContactsContract.Contacts._ID,
+        String[] projection = {ContactsContract.Contacts._ID,
                 ContactsContract.Contacts.LOOKUP_KEY,
                 ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
                 ContactsContract.Contacts.PHOTO_THUMBNAIL_URI,
-                sortOrder };
+                sortOrder};
         String selection = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + "<>''" + " AND " + ContactsContract
                 .Contacts.IN_VISIBLE_GROUP + "=1";
 
@@ -88,13 +83,12 @@ public class ContactHelper {
 
     private Cursor getPHoneCursor(String contactId) {
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String[] projection = new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER };
+        String[] projection = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
         String selection = ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId;
 
         return mContext.getContentResolver().query(uri, projection, selection, null, null);
     }
-
 
 
     private Cursor getEmailsCursor(String contactId) {
